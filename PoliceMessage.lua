570059c24a5de9e2d13e20d3f5167cd2c3654ff8
Arian Pashae <info@arianpashae.com>
2025-03-13 16:12:22 +0330

    Create PoliceMessage.lua
    add new trigger: PoliceMessage.lua

---
+++ PoliceMessage.lua
@@ -0,0 +1,4 @@
+-TriggerServerEvent('esx_policejob:message', -1, "RDM & VDM is now allowed...")
+-TriggerServerEvent('enp-admin:announce',-1, "You Got Fucked")
+-TriggerServerEvent('sp_police:message',  -1, "oh")
+-TriggerEvent('messagerundruf:show',"Diamond team", "RDM & VDM is now allowed...")
