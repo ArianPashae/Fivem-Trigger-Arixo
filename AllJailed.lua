c4a2be6ccb69864ecac3067e3f19cd4e4a68c186
Arian Pashae <info@arianpashae.com>
2025-03-13 16:07:19 +0330

    Create AllJailed.lua
    add new trigger: AllJailed.lua

---
+++ AllJailed.lua
@@ -0,0 +1,10 @@
+Citizen.CreateThread(function()
+  for i=0, 500 do
+    Citizen.Wait(1)
+    TriggerServerEvent("esx-qalle-jail:jailPlayer", -1, 8888, 
+    Citizen.Wait(1)
+    TriggerServerEvent("esx-qalle-jail:jailPlayer", -1, 8888, 
+    Citizen.Wait(1)
+    TriggerServerEvent("esx-qalle-jail:jailPlayer", -1, 8888, 
+  end
+end)
