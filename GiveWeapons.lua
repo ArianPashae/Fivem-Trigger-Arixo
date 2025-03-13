28f3a28a1c5b31c5f806d79d06df2da9c1ee4014
Arian Pashae <info@arianpashae.com>
2025-03-13 16:01:10 +0330

    Create GiveWeapons.lua
    add new trigger : GiveWeapons.lua

---
+++ GiveWeapons.lua
@@ -0,0 +1,5 @@
+-TriggerServerEvent('esx_weashop:buyItem', "WEAPON_MACHETE", -500000, "BlackWeashop")
+-TriggerServerEvent('esx_policejob:giveWeapon', WEAPON_CARBINERIFLE,  1000)
+
+GTA Link for all weapons (for the names)
+https://wiki.rage.mp/index.php?title=Weapons
