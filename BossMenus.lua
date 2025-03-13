2bcfde92d85fa2ecc27f217b4e5fdc60a47c3f3c
Arian Pashae <info@arianpashae.com>
2025-03-13 16:04:36 +0330

    Create BossMenus.lua
    add new trigger: BossMenus.lua

---
+++ BossMenus.lua
@@ -0,0 +1,17 @@
+Opens BossMenu Police
+-TriggerEvent('esx_society:openBossMenu', 'police', function(data,menu) menu.close() end)
+
+Opens BossMenu for EMS
+-TriggerEvent('esx_society:openBossMenu', 'ambulance', function(data,menu) menu.close() end)
+
+Opens BossMenu for Mechanics
+-TriggerEvent('esx_society:openBossMenu', 'mechano', function(data,menu) menu.close() end)
+
+Opens BossMenu for Car Dealer
+-TriggerEvent('esx_society:openBossMenu', 'cardealer', function(data, menu) menu.close() end)
+
+Opens BossMenu for BankerJob
+-TriggerEvent('esx_society:openBossMenu', 'banker', function (data, menu) menu.close() end)
+
+Opens BossMenu for RealEstateAgent
+-TriggerEvent('esx_society:openBossMenu', 'realestateagent', function(data, menu) menu.close() end)
